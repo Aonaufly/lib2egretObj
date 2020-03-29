56fd3b2476871bf542ab0dffc5c37e01f9db2cde
Aonaufly <aonaufly@126.com>
2020-03-29 10:57:59 +0800

    封装socket实体解析

--- src/network/socket/distribute/BaseSocketDistribute.ts
+++ src/network/socket/distribute/BaseSocketDistribute.ts
@@ -11,18 +11,23 @@ module lib2egret.network {
      * @author Aonaufly
      */
     export abstract class BaseSocketDistribute<HEAD extends ISocketHead> implements common.IDestroy {
+        /**实体内容解析器 */
         protected _bodyAnalysis: BaseSocketBodyAnalysis<HEAD>;
+        /**实体 */
+        protected _entity: any;
 
         public constructor($bodyAnalysis: IBaseSocketBodyAnalysis<HEAD>, $config: egret.XMLNode | JSON) {
             this._bodyAnalysis = new $bodyAnalysis($config);
         }
 
         /**
-         * 分发消息（需要解析成Model）
+         * 分发消息（需要解析成实体）
          * @param $head 包头
          * @param $body 包体
          */
-        public abstract distribute($head: HEAD, $body: SByteArray): void;
+        public distribute($head: HEAD, $body: SByteArray): void {
+            this._entity = this._bodyAnalysis.execute2Plaintext($head, $body);//解析出Body
+        }
 
         /**
          * @inheritDoc
